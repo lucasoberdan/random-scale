fe8ca03cfe354c242801afda5e23f030164f4dd3
Lucas Oberdan <74725641+lucasoberdan@users.noreply.github.com>
2022-06-19 21:56:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
-//Quais as variáveis
-//Quais os dados de entrada?
-//Quais os dados de saída?
+const elementoResposta = document.querySelector("#resposta");
 
-const elementoResposta = document.querySelector("#resposta")
+const escalasMaiores = [
+  "Dó&#9837 Maior",
+  "Sol&#9837 Maior",
+  "Ré&#9837 Maior",
+  "Lá&#9837 Maior",
+  "Mi&#9837 Maior",
+  "Si&#9837 Maior",
+  "Fá Maior",
+  "Dó Maior",
+  "Sol Maior",
+  "Ré Maior",
+  "Lá Maior",
+  "Mi Maior",
+  "Si Maior",
+  "Fá&#9839 Maior",
+  "Dó&#9839 Maior",
+];
 
-const escalaBemol = [
-  "Fá",
-  "Sol &#9837",
-  "Lá &#9837",
-  "Si &#9837",
-  "Dó &#9837",
-  "Ré &#9837",
-  "Mi &#9837",
-]
+const escalasMenores = [
+  "Lá&#9837 Menor",
+  "Mi&#9837 Menor",
+  "Si&#9837 Menor",
+  "Fá Menor",
+  "Dó Menor",
+  "Sol Menor",
+  "Ré Menor",
+  "Lá Menor",
+  "Mi Menor",
+  "Si Menor",
+  "Fá&#9839 Menor",
+  "Dó&#9839 Menor",
+  "Sol&#9839 Menor",
+  "Ré&#9839 Menor",
+  "Lá&#9839 Menor",
+];
 
-const escalaSustenido = [
-  "Sol",
-  "Lá",
-  "Si",
-  "Dó &#9839",
-  "Ré",
-  "Mi",
-]
+function escolherEscalaMaior() {
+  escalaMaior = true; // Sem palavra reservada para poder chamar ele fora da função
+  const totalEscalasMaiores = escalasMaiores.length;
+  numeroAleatorioMaiores = Math.floor(Math.random() * totalEscalasMaiores); // Sem palavra reservada para poder chamar ele fora da função
 
-const respostas = [
-  "Certeza!",
-  "Não tenho tanta certeza.",
-  "É decididamente assim.",
-  "Não conte com isso.",
-  "Sem dúvidas!",
-  "Pergunte novamente mais tarde.",
-  "Sim, definitivamente!",
-  "Minha resposta é não.",
-  "Você pode contar com isso.",
-  "Melhor não te dizer agora.",
-  "A meu ver, sim.",
-  "Minhas fontes dizem não.",
-  "Provavelmente.",
-  "Não é possível prever agora.",
-  "Perspectiva boa.",
-  "As perspectivas não são tão boas.",
-  "Sim.",
-  "Concentre-se e pergunte novamente.",
-  "Sinais apontam que sim.",
-]
-
-function escolherEscalaB() {
-  const totalEscalasB = escalaBemol.length
-  const numeroAleatorioB = Math.floor(Math.random() * totalEscalasB)
-
-  elementoResposta.innerHTML = escalaBemol[numeroAleatorioB]
+  elementoResposta.innerHTML = escalasMaiores[numeroAleatorioMaiores];
 }
 
-function escolherEscalaS() {
-  const totalEscalasS = escalaSustenido.length
-  const numeroAleatorioS = Math.floor(Math.random() * totalEscalasS)
+function escolherEscalaMenor() {
+  escalaMenor = true; // Sem palavra reservada para poder chamar ele fora da função
+  const totalEscalasMenores = escalasMenores.length;
+  numeroAleatorioMenores = Math.floor(Math.random() * totalEscalasMenores); // Sem palavra reservada para poder chamar ele fora da função
 
-  elementoResposta.innerHTML = escalaSustenido[numeroAleatorioS]
+  elementoResposta.innerHTML = escalasMenores[numeroAleatorioMenores];
+}
+
+function escolherEscalaRelativa() {
+  if (escalaMaior == true) {
+    elementoResposta.innerHTML = escalasMenores[numeroAleatorioMaiores];
+  } else if (escalaMenor == true) {
+    elementoResposta.innerHTML = escalasMaiores[numeroAleatorioMenores];
+  }
 }
